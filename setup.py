0966f6ad6722b18538476c867c1165c88c9aa463
Gátomo <Gatomociencia@gmail.com>
2023-04-29 20:44:22 +0200

    Version 1.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'uncolor',
     packages = ['uncolor'],
-    version = '1.0',
+    version = '1.1',
     description = '🎨 | Uncomplicated colorizing tool',
     author = 'Gátomo',
     author_email='dontSendMeAnEmail@thanks.com',
     url = 'https://github.com/gatomo-oficial/uncolor',
-    download_url = 'https://github.com/gatomo-oficial/uncolor/archive/refs/tags/1.0.tar.gz',
+    download_url = 'https://github.com/gatomo-oficial/uncolor/archive/refs/tags/1.1.tar.gz',
     keywords = ['Terminals','Color', 'Colorize', 'Uncolor', 'Styling'],
     classifiers = [],
 )
